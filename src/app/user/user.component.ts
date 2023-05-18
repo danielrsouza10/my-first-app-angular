0c9ab6222dfe4ce2763d3333d8df3a8337e895dd
Daniel Souza <danielrsouza10@gmail.com>
2023-05-18 16:44:39 -0300

    Practicing Databinding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,35 +1,67 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styles: [
+    `
+      .displayPressed {
+        background-color: darkgreen;
+      }
+    `,
+  ],
 })
 export class UserComponent {
-  username: string = '';
+  // username: string = "";
+  // status: boolean = false;
+  // userList = ["test-user"];
 
-  allowNewUser = false;
-  userCreationStatus = 'No user was create';
+  detailList: string = "";
+  button: string = "Display Details";
+  controlVar = ["test"];
+  i = 0;
+  // allowNewUser = false;
+  // userCreationStatus = "No user was create";
 
   constructor() {
-    setTimeout(() => {
-      this.allowNewUser = true;
-    }, 1000);
+    // setTimeout(() => {
+    //   this.allowNewUser = true;
+    // }, 1000);
   }
 
-  onCreateUser() {
-    if (this.username != '') {
-      this.userCreationStatus = 'User was created: Welcome, ' + this.username;
+  onDetailListButton() {
+    this.detailList = "Lorem upsiluim";
+    if (this.button === "Display Details") {
+      this.button = "Hide Details";
+      this.controlVar.push("again");
+      this.i++;
     } else {
-      this.userCreationStatus = 'You need to type a username';
+      this.button = "Display Details";
     }
   }
 
-  onResetButton() {
-    this.username = '';
-    this.userCreationStatus = 'No user was create';
-  }
+  //   onCreateUser() {
+  //     if (this.username != "") {
+  //       this.userCreationStatus =
+  //         "User was created: Welcome, " + this.username + "!";
+  //       this.userList.push(this.username);
+  //       this.status = true;
+  //     } else {
+  //       this.userCreationStatus = "You need to type a username";
+  //     }
+  //   }
 
-  onUserType(event: Event) {
-    this.username = (<HTMLInputElement>event.target).value;
+  //   onResetButton() {
+  //     this.username = "";
+  //     this.userCreationStatus = "No user was create";
+  //     this.status = false;
+  //   }
+
+  //   onUserType(event: Event) {
+  //     this.username = (<HTMLInputElement>event.target).value;
+  //   }
+
+  getColor() {
+    return this.i >= 5 ? "white" : "black";
   }
 }
